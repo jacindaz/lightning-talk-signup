e6fb83bd0a5ab8cc11442cfe2e90ddc088848803
Jacinda Zhong <jacindaz@gmail.com>
2014-06-21 11:58:30 -0400

    Checks if there are duplicates before saving to db
     - is_dupe? returns true or false if a topic exists, or if
       the first and last name exists in db
     - is_empty? returns boolean if any fields are empty

---
+++ helpers.rb
@@ -0,0 +1,69 @@
+require 'pg'
+require 'pry'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'lightning-talks_development')
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+def return_all_talks
+  all_talks = "SELECT * FROM talks"
+  talks = db_connection do |conn|
+              conn.exec_params(all_talks)
+            end
+end
+
+def query_db(query, params)
+
+  db_connection do |conn|
+    conn.exec_params(query, [params])
+  end
+end
+
+def save_to_db(first, last, topic)
+  insert = "INSERT INTO talks (first_name, last_name, talk_title, created_at)
+              VALUES ($1, $2, $3, now())"
+  @insert_db = db_connection do |conn|
+              conn.exec_params(insert, [first, last, talk])
+            end
+end
+
+def is_empty?(first, last, topic)
+  return (!first.empty? && !last.empty? && !topic.empty?)
+end
+
+def remove_double_quote(string)
+  if string
+  end
+end
+
+#true if no duplicates, false if is a dupe
+def is_dupe?(first, last, topic)
+  all_talks = return_all_talks
+  first_exist = false
+  last_exist = false
+  topic_exist = false
+  return_all_talks.each do |talk|
+    if talk["first_name"] == first
+      first_exist = true
+    end
+    if talk["last_name"] == last
+      last_exist = true
+    end
+    if talk["talk_title"] == topic
+      topic_exist = true
+    end
+    if (topic_exist || (first_exist && last_exist)) == true
+      return true
+    end
+  end
+  false
+end
+
+#puts is_dupe?("Jacinda", "Blah", "lala")       # return false
+#puts is_dupe?('Jacinda', 'Zhong', 'lala')       # return true
+
--- server.rb
+++ server.rb
@@ -3,42 +3,45 @@ require 'rubygems'
 require 'pry'
 require 'pg'
 
-
-
-#METHODS--------------------------------------------------------------------------------
-def db_connection
-  begin
-    connection = PG.connect(dbname: 'lightning-talks_development')
-
-    yield(connection)
-
-  ensure
-    connection.close
-  end
-end
-
+require_relative 'helpers.rb'
 
 #ROUTES and VIEWS--------------------------------------------------------------------------------
 
-
 get '/' do
-
-  all_talks = "SELECT * FROM talks"
-
-  actor_query = "SELECT actors.id,actors.name,
-                    movies.title AS movie, movies.id AS movie_id,
-                    cast_members.character AS role
-                 FROM actors
-                    JOIN cast_members ON actors.id = cast_members.actor_id
-                    JOIN movies ON cast_members.movie_id = movies.id
-                  WHERE actors.id = $1"
-
-  @talks = db_connection do |conn|
-              conn.exec_params(all_talks)
-            end
-
+  @name = params[:username]
+  @topic = params[:usertalktopic]
+  @all_talks = return_all_talks
   erb :index
 end
+
+post '/add_talk' do
+  @first_name = params["firstname"]
+  @last_name = params["lastname"]
+  @talk_title = params["usertalktopic"]
+
+  if is_empty?(@first_name, @last_name, @talk_title) && is_dupe?(@first_name, @last_name, @talk_title)
+    save_to_db(@first_name, @last_name, @talk_title)
+    redirect '/'
+  else
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
