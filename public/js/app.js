f45c99929a3c7474406c702fb53eebe6cdd71643
Jacinda Zhong <jacindaz@gmail.com>
2014-06-23 21:11:29 -0400

    Custom icons for jQuery accordion

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,4 @@
 
-
-$( '#one-talk-id-la' ).click(function() {
-  alert("inside one-talk-id alert")
-  toggle(function() {
-    document.getElementById("span-icon-id").className = "glyphicon glyphicon-chevron-right"
-  }, function() {
-    document.getElementById("span-icon-id").className = "glyphicon glyphicon-chevron-down"
-  });
-});
-
-$( "#target" ).toggle(function() {
-  alert( "First handler for .toggle() called." );
-}, function() {
-  alert( "Second handler for .toggle() called." );
-});
-
 function thanksAlert() {
   var userName = document.sign_up.input_first_name.value;
   alert("Thanks " + userName + " for submitting a lightning talk!");
